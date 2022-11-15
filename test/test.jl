6b168e457743e0c8aee664216556c076e62b6b51
“jeffersonfparil” <jeffersonparil@gmail.com>
2022-11-15 06:18:18 +0000

    reimplementing ABC and testing RandomForest

--- test/test.jl
+++ test/test.jl
@@ -6,7 +6,6 @@ if githubci
     using Pkg
     Pkg.add(url="https://github.com/jeffersonfparil/poolgen.git")
     using poolgen ### Load poolgen first so we can compile now and no precompilation for each process
-    using Distributed
     Distributed.addprocs(length(Sys.cpu_info())-1)
     @everywhere using poolgen ### Load poolgen for each process
 else
