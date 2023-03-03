76f09a2763e84493cece5fb74f74f37c61a91b68
jeffersonfparl <jeffersonfparil@gmail.com>
2023-03-03 06:41:44 +0000

    working on regression now

--- src/regression/ols.rs
+++ src/regression/ols.rs
@@ -1,2 +1,9 @@
-println!("Hello there!");
-println!("General Kenobi!");
+use std::io::{self, prelude::*, Error, ErrorKind, BufReader};
+use nalgebra::{self, DMatrix};
+use std::sync::{Arc, Mutex};
+use std::fs::{File, OpenOptions};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use crate::io::sync::{Sync, sync_analyser_and_writer_single_thread};
+use crate::io::sync::AlleleCountsOrFrequencies;
+use crate::io::phen::{Phenotypes, load_phen};
