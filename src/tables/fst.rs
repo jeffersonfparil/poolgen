ed0b359592727f97255fd276b531f8cc92d60714
jeffersonfparl <jeffersonparil@gmail.com>
2023-05-31 10:10:45 +0000

    starting to implement Gautiers Fst

--- src/tables/fst.rs
+++ src/tables/fst.rs
@@ -1,61 +1,79 @@
+use std::io;
 use crate::base::*;
 use ndarray::prelude::*;
 use statrs::distribution::{ChiSquared, ContinuousCDF};
 
-pub fn fst(locus_counts: &mut LocusCounts, filter_stats: &FilterStats) -> Option<String> {
-    println!("locus_counts={:?}", locus_counts);
-    let locus_counts = match locus_counts.filter(filter_stats) {
-        Ok(x) => x,
-        Err(_) => return None,
-    };
-    let locus_frequencies = match locus_counts.to_frequencies() {
-        Ok(x) => x,
-        Err(_) => return None,
-    };
 
-    // Using Gautier et al, 2021 estimates (https://doi.org/10.1111/1755-0998.13557)
-    println!("locus_frequencies={:?}", locus_frequencies);
+pub fn ols_with_covariate(
+    genotypes_and_phenotypes: &GenotypesAndPhenotypes,
+    xxt_eigen_variance_explained: f64,
+    fname_input: &String,
+    fname_output: &String,
+) -> io::Result<String> {
+    // View the genotype matrix
+    let g = genotypes_and_phenotypes
+        .intercept_and_allele_frequencies
+        .slice(s![0.., 1..]);
+    let (n, p) = (g.nrows(), g.ncols());
 
-    let counts_per_pool_per_allele: Array2<f64> = locus_counts.matrix.map(|&y| y as f64);
-    let total_counts_per_pool: Array1<f64> = counts_per_pool_per_allele.sum_axis(Axis(1));
-    let (n, p) = counts_per_pool_per_allele.dim();
+    // Count the number of loci
+    let mut loci: Vec<String> = vec![];
+    for i in 0..p {
+        let locus = genotypes_and_phenotypes.chromosome[i].clone() + &genotypes_and_phenotypes.position[i].to_string();
+        if loci.len() > 0 {
+            if locus != loci[loci.len()-1] {
+                loci.push(locus);
+            }
+        } else {
+            loci.push(locus);
+        }
+    }
 
-    let i = 213; // locus index
-    let a = 0; // arbitrary reference allele index
-    let j = 0; // pool i index
-    let k = 1; // pool k index
 
-    let q_1i_j = 1.00 - (2.00 * 
-        (counts_per_pool_per_allele[(j,a)]/total_counts_per_pool[j]) * 
-        ((total_counts_per_pool[j]-counts_per_pool_per_allele[(j,a)]) / (total_counts_per_pool[j] - 1.00))
-    );
+
+    // println!("locus_counts={:?}", locus_counts);
+    // let locus_counts = match locus_counts.filter(filter_stats) {
+    //     Ok(x) => x,
+    //     Err(_) => return None,
+    // };
+    // let locus_frequencies = match locus_counts.to_frequencies() {
+    //     Ok(x) => x,
+    //     Err(_) => return None,
+    // };
+
+    // // Using Gautier et al, 2021 estimates (https://doi.org/10.1111/1755-0998.13557)
+    // println!("locus_frequencies={:?}", locus_frequencies);
+
+    // let counts_per_pool_per_allele: Array2<f64> = locus_counts.matrix.map(|&y| y as f64);
+    // let total_counts_per_pool: Array1<f64> = counts_per_pool_per_allele.sum_axis(Axis(1));
+    // let (n, p) = counts_per_pool_per_allele.dim();
+
+    // let i = 213; // locus index
+    // let a = 0; // arbitrary reference allele index
+    // let j = 0; // pool i index
+    // let k = 1; // pool k index
+
+    // let q_1i_j = 1.00 - (2.00 * 
+    //     (counts_per_pool_per_allele[(j,a)]/total_counts_per_pool[j]) * 
+    //     ((total_counts_per_pool[j]-counts_per_pool_per_allele[(j,a)]) / (total_counts_per_pool[j] - 1.00))
+    // );
     
-    let q_1i_k = 1.00 - (2.00 * 
-        (counts_per_pool_per_allele[(k,a)]/total_counts_per_pool[k]) * 
-        ((total_counts_per_pool[k]-counts_per_pool_per_allele[(k,a)]) / (total_counts_per_pool[k] - 1.00))
-    );
+    // let q_1i_k = 1.00 - (2.00 * 
+    //     (counts_per_pool_per_allele[(k,a)]/total_counts_per_pool[k]) * 
+    //     ((total_counts_per_pool[k]-counts_per_pool_per_allele[(k,a)]) / (total_counts_per_pool[k] - 1.00))
+    // );
 
-    let q_2i_j = (1.00 / (total_counts_per_pool[j]*total_counts_per_pool[k])) * 
-    ( counts_per_pool_per_allele[(j,a)]*counts_per_pool_per_allele[(k,a)] + 
-        (total_counts_per_pool[j]-counts_per_pool_per_allele[(j,a)]) 
-            * (total_counts_per_pool[k]-counts_per_pool_per_allele[(k,a)])
-    );
+    // let q_2i_j = (1.00 / (total_counts_per_pool[j]*total_counts_per_pool[k])) * 
+    // ( counts_per_pool_per_allele[(j,a)]*counts_per_pool_per_allele[(k,a)] + 
+    //     (total_counts_per_pool[j]-counts_per_pool_per_allele[(j,a)]) 
+    //         * (total_counts_per_pool[k]-counts_per_pool_per_allele[(k,a)])
+    // );
 
-    let fst_i_jk = ((0.5*(q_1i_j+q_1i_k)) - q_2i_j) / (1.00 + q_2i_j);
-    println!("fst_i_jk = {:?}", fst_i_jk);
+    // let fst_i_jk = ((0.5*(q_1i_j+q_1i_k)) - q_2i_j) / (1.00 + q_2i_j);
+    // println!("fst_i_jk = {:?}", fst_i_jk);
 
-    // Output line
-    // let out = vec![
-    //     locus_frequencies.chromosome.clone(),
-    //     locus_frequencies.position.to_string(),
-    //     locus_frequencies.alleles_vector.join(""),
-    //     parse_f64_roundup_and_own(chi2, 6),
-    //     pval.to_string(),
-    // ]
-    // .join(",")
-    //     + "\n";
-    // Some(out)
-    Some(fst_i_jk.to_string())
+    // Some(fst_i_jk.to_string())
+    Ok("0".to_string())
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -87,9 +105,9 @@ mod tests {
             // pool_sizes: vec![0.2, 0.2, 0.2, 0.2],
             pool_sizes: vec![0.5, 0.5],
         };
-        // Outputs
-        let out = fst(&mut locus_counts, &filter_stats).unwrap();
-        // Assertions
-        assert_eq!("1".to_owned(), out);
+        // // Outputs
+        // let out = fst(&mut locus_counts, &filter_stats).unwrap();
+        // // Assertions
+        // assert_eq!("1".to_owned(), out);
     }
 }
