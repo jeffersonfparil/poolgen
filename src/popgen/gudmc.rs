5b574486508a6a509ec31c47f320c707084f2360
jeffersonfparil <jeffersonparil@gmail.com>
2023-10-04 06:44:48 +0000

    testing gudmc

--- src/popgen/gudmc.rs
+++ src/popgen/gudmc.rs
@@ -92,7 +92,7 @@ pub fn gudmc(
         &(PARAMETER_UPPER_LIMIT as usize),
     )
     .unwrap();
-    // println!("tajima_row_labels={:?}", tajima_row_labels);
+    println!("tajima_row_labels={:?}", tajima_row_labels);
     // println!("tajima_col_labels={:?}", tajima_col_labels);
     // println!("tajima={:?}", tajima);
     /////////////////////////////////////////////////////////
@@ -285,7 +285,7 @@ pub fn gudmc(
     'outer: for i in 0..fst_pop_a.len() {
         let a = fst_pop_a[i].clone();
         let b = fst_pop_b[i].clone();
-        println!("a={:?}; b={:?}", a, b);
+        // println!("a={:?}; b={:?}", a, b);
         let idx_tajima = match tajima_pop.iter().position(|x| x == &b) {
             Some(x) => x,
             None => continue 'outer,
@@ -311,7 +311,7 @@ pub fn gudmc(
                 + &tajima_pos_ini[idx_tajima][j].to_string()
                 + "-"
                 + &tajima_pos_fin[idx_tajima][j].to_string();
-            println!("tajima_window_id={:?}", tajima_window_id);
+            // println!("tajima_window_id={:?}", tajima_window_id);
             let idx_fst = match (0..fst_chr[i].len()).position(|idx| {
                 tajima_window_id
                     == fst_chr[i][idx].to_owned()
@@ -323,7 +323,7 @@ pub fn gudmc(
                 Some(x) => x,
                 None => continue 'inner,
             };
-            println!("idx_fst={:?}", idx_fst);
+            // println!("idx_fst={:?}", idx_fst);
             chr[i].push(tajima_chr[idx_tajima][j].to_owned());
             pos_ini[i].push(tajima_pos_ini[idx_tajima][j]);
             pos_fin[i].push(tajima_pos_fin[idx_tajima][j]);
