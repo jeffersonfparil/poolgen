b9f3c528f569a41ecd0bef05f88e06b7b3698816
cjdjpj <cjdjpj@hotmail.com>
2024-06-27 17:49:23 +1000

    feat: min_coverage_breadth fix: outdated tests
    replaced keep_if_any_meets_coverage with min_coverage_breadth, which
    allows specifying frequency of pools that need to meet coverage
    requirement. (renamed min_coverage to min_coverage_depth for clarity)
    
    made tests up to date with new flags

--- src/imputation/adaptive_ld_knn_imputation.rs
+++ src/imputation/adaptive_ld_knn_imputation.rs
@@ -497,8 +497,11 @@ mod tests {
         let file_sync_phen = *(file_sync, file_phen).lparse().unwrap();
         let filter_stats = FilterStats {
             remove_ns: true,
+            remove_monoallelic: false,
+            keep_lowercase_reference: false,
             max_base_error_rate: 0.005,
-            min_coverage: 1,
+            min_coverage_depth: 1,
+            min_coverage_breadth: 1.0,
             min_allele_frequency: 0.005,
             max_missingness_rate: 0.0,
             pool_sizes: vec![20., 20., 20., 20., 20.],
